1e11dec89405fbf54a9db1ef80e5ead6bcdef790
Gatos90 <verweyen.kevin@gmail.com>
2023-10-09 16:52:35 +0200

    Quest

--- src/main/java/com/example/thymeleaf/controller/DoctorController.java
+++ src/main/java/com/example/thymeleaf/controller/DoctorController.java
@@ -1,14 +1,25 @@
 package com.example.thymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DoctorController {
 
     @GetMapping("/doctor")
-    public String doctor() {
+    public String doctor(Model model,
+            @RequestParam(value = "name", defaultValue = "John Smith") String name,
+            @RequestParam(value = "number", defaultValue = "0") int number) {
+
+        String doctorName = name;
+        int doctorNumber = number;
+
+        model.addAttribute("doctorname", doctorName);
+        model.addAttribute("doctornumber", doctorNumber);
 
         return "doctor";
     }
+
 }
